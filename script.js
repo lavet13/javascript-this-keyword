584026a247ec20526393554aa4758c11044f2056
lavet13 <lavet13@mail.ru>
2022-07-08 14:12:54 +0300

    almost end up with this section

--- script.js
+++ script.js
@@ -6,12 +6,12 @@
 
 // function first() {
 //     const age = 30;
-    
+
 //     if(age >= 30) {
 //         const decade = 3;
 //         var millenial = true;
 //     }
-    
+
 //     function second() {
 //         const job = "teacher";
 //         console.log(`${myName} is a ${age}-old ${job}`);
@@ -32,15 +32,15 @@
 
 //         if(birthYear >= 1981 && birthYear <= 1996) {
 //             var millenial = true;
-//             // Creating NEW variable with the same name as outer scope's variable 
+//             // Creating NEW variable with the same name as outer scope's variable
 //             const firstName = () => 'Steven';
-            
+
 //             // Reassigning outer scope's variable
 //             output = 'NEW OUTPUT!';
 
 //             const str = `Oh, and you're a millenial, ${firstName()}`;
 //             console.log(str);
-            
+
 //             function add (a, b) { // only in strict mode
 //                 return a + b;
 //             }
@@ -68,7 +68,6 @@
 
 // using var or let or const either with the function expression or arrow function
 
-
 // const myName = 'Jonas';
 
 // if(myName === 'Jonas') {
@@ -78,12 +77,10 @@
 //     const age = 2037 - 1989;
 //     console.log(age);
 //     // the end of a TDZ(Temporal Dead Zone)
-//     // before initialization 
+//     // before initialization
 //     const job = 'teacher';
 //     console.log(x);
 // }
-
-
 
 // Example 4
 /*
@@ -115,7 +112,6 @@ var addArrow = (a, b) => a + b;
 
 */
 
-
 /*
 
 // Example 5
@@ -140,7 +136,6 @@ console.log(y === window.y);
 console.log(z === window.z);
 
 */
-
 
 // Example 6
 
@@ -208,42 +203,111 @@ f();
 
 // var firstName = 'Matilda'; // creates a property in the window object which is equals to undefined
 
-
 const jonas = {
-    firstName: 'Jonas',
-    year: 1991,
-    calcAge: function() {
-        console.log(this);
-        console.log(2037 - this.year);
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
 
-        try {  
+    try {
+      /*
+            // regular function inside of a method didn't get the this keyword from the parent scope
             // const isMillenial = function() {
+            //     console.log(this);
             //     console.log(this.year >= 1981 && this.year <= 1996);
             // };
             
-            // function isMillenial() {
-            //     console.log(this.year >= 1981 && this.year <= 1996);
-            // }
+            */
 
-            const isMillenial = () => console.log(this.year >= 1981 && this.year <= 1996);
+      /*
 
-            isMillenial();
-            
-        } catch(e) {
-            if(e instanceof TypeError) {
-                console.log('TypeError is occured');
+            // regular function inside of a method didn't get the this keyword from the parent scope
+            // Solution 1 to the this keyword
+            const self = this; // the way we can preserve the this keyword, this can also be called "that", so that I might also see.
+            function isMillenial() {
+                console.log(self);
+                console.log(self.year >= 1981 && self.year <= 1996);
             }
-        }
-        
-    },
 
-    greet: () => { 
-        // pitfall, because it's getting the global object which is the window object
-        console.log(`Hey ${this.firstName}`); 
-    }, 
+            */
+
+      // Solution 2, which doesn't need this extra variable
+      const isMillenial = () => {
+        console.log(this);
+        console.log(this.year >= 1981 && this.year <= 1996);
+      };
+
+      isMillenial();
+    } catch (e) {
+      if (e instanceof TypeError) {
+        console.log(`${e.message}`);
+      }
+    }
+  },
+
+  greet: () => {
+    // pitfall, because it's getting the global object which is the window object
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
 };
 
-jonas.greet();
-
+// jonas.greet();
 
 jonas.calcAge();
+
+/*
+// arguments keyword
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
+};
+
+addExpr(2, 3);
+addExpr(2, 3, 8, 12);
+
+const addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+};
+
+addArrow(2, 3);
+
+
+/*
+// you wouldn't get access to a function outside of the block scope if it's working in strict mode
+if(1) {
+    let test1 = "test1";
+    const test2 = "test2";
+
+    function test() {
+        console.log('test');
+    }
+}
+
+test();
+
+console.log(test1, test2);
+*/
+/*
+
+// subtle distinction between primitive types and reference types
+
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age, oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+
+console.log('Me:', me);
+console.log('Friend:', friend);
+*/
